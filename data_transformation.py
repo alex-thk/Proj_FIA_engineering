1d8824fd2c43ac4d12d36c393e45eadefb798616
Alessandro Gallo <2002alexgallo@gmail.com>
2024-09-03 18:07:56 +0200

    creazione del mapping da valori categorici a valori numerici, necessario per poter semplificare ulteriormente il dataset e proseguire con la creazione delle dummy variables.

--- data_transformation.py
+++ data_transformation.py
@@ -2,9 +2,15 @@ import pandas as pd
 
 
 class DataTransformation:
-    def __init__(self, dataframe):
-        self.df = dataframe
+    def __init__(self, df):
+        self.df = df
 
-    def transform_data(self):
-        # transformation logic goes here
-        pass
+    def transform_col(self, column, mapping):
+        self.df[column + '_tuple'] = list(zip(self.df[column], self.df[column].map(mapping)))
+        self.df[column] = self.df[column + '_tuple']
+        print(self.df[column].head())
+
+    def create_dummies(self, column):
+        dummies = pd.get_dummies(self.df[column], prefix=column)
+        self.df = pd.concat([self.df, dummies], axis=1)
+        self.df.drop(columns=[column], inplace=True)
--- main.py
+++ main.py
@@ -20,3 +20,35 @@ if __name__ == '__main__':
     reducer.remove_insignificant_columns()
     cleaner.calculate_precentage_missing_values_in_df() # should print no missing values
     print(df.info())
+
+    # per fare 'nord sud centro' toglieremo
+    # 'regione_residenza, asl_residenza, provincia_residenza, comune_residenza'
+    print(df['codice_tipologia_professionista_sanitario'].unique())
+
+    # assistenza sanitaria diretta 1
+    # infermiere, ostetrica, fisioterapista, podologo
+
+    # assistenza riabilitativa e terapeutica psico-sociale 2
+    # psicologo, tecnico riabilitazione psichiatrica, terapista occupazionale, terapista della neuro e psicomotricita dell'eta evolutiva
+
+    # supporto salute e benessere 3
+    # dietista, educatore professionale, assistente sanitario, logopedista
+
+    # mapping for codice_tipologia_professionista_sanitario
+    mapping1 = {
+        'INF': 1, 'OST': 1, 'FIS': 1, 'POD': 1,
+        'PSI': 2, 'TRP': 2, 'TRO': 2, 'TNP': 2,
+        'DIE': 3, 'EDP': 3, 'ASN': 3, 'LPD': 3
+    }
+
+    transformer = DataTransformation(df)
+    transformer.transform_col('codice_tipologia_professionista_sanitario', mapping1)
+
+    region_mapping = {
+        "Piemonte": 1, "Valle d'Aosta": 1, "Liguria": 1, "Lombardia": 1, "Trentino-Alto Adige": 1, "Veneto": 1, "Friuli venezia giulia": 1, "Emilia Romagna": 1,
+        "Toscana": 2, "Umbria": 2, "Marche": 2, "Lazio": 2,
+        "Abruzzo": 3, "Molise": 3, "Campania": 3, "Puglia": 3, "Basilicata": 3, "Calabria": 3, "Sicilia": 3, "Sardegna": 3
+    }
+
+    transformer.transform_col('regione_residenza', region_mapping)
+    transformer.transform_col('regione_erogazione', region_mapping)
