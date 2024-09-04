1cdedfc8c6c566d33ac3279b60c7e6a08c1b5410
Alessandro Gallo <2002alexgallo@gmail.com>
2024-09-04 09:52:51 +0200

    perfezionamento dei mapping delle colonne in vista dell'utilizzo di dummy variables

--- data_transformation.py
+++ data_transformation.py
@@ -5,10 +5,13 @@ class DataTransformation:
     def __init__(self, df):
         self.df = df
 
-    def transform_col(self, column, mapping):
+    def transform_col(self, column, mapping, new_col_name):
         self.df[column + '_tuple'] = list(zip(self.df[column], self.df[column].map(mapping)))
         self.df[column] = self.df[column + '_tuple']
-        print(self.df[column].head())
+        # lambda function extracts the second element of the tuple and apply applies it to the column
+        self.df[new_col_name] = self.df[column].apply(lambda x: x[1])
+
+        print(self.df[[column, new_col_name]].head())
 
     def create_dummies(self, column):
         dummies = pd.get_dummies(self.df[column], prefix=column)
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ if __name__ == '__main__':
     file = 'challenge_campus_biomedico_2024.parquet'
     df = pd.read_parquet(file)
 
+    lista = df['regione_residenza'].unique()
+
     cleaner = DataCleaning(df)
     cleaner.calculate_precentage_missing_values_in_df()
     cleaner.handle_last_column()
@@ -19,11 +21,11 @@ if __name__ == '__main__':
     reducer = DataReduction(df)
     reducer.remove_insignificant_columns()
     cleaner.calculate_precentage_missing_values_in_df() # should print no missing values
-    print(df.info())
+    # print(df.info())
 
     # per fare 'nord sud centro' toglieremo
     # 'regione_residenza, asl_residenza, provincia_residenza, comune_residenza'
-    print(df['codice_tipologia_professionista_sanitario'].unique())
+    # print(df['codice_tipologia_professionista_sanitario'].unique())
 
     # assistenza sanitaria diretta 1
     # infermiere, ostetrica, fisioterapista, podologo
@@ -42,13 +44,19 @@ if __name__ == '__main__':
     }
 
     transformer = DataTransformation(df)
-    transformer.transform_col('codice_tipologia_professionista_sanitario', mapping1)
+    transformer.transform_col('codice_tipologia_professionista_sanitario', mapping1, 'group_professionisti')
+
+    # NORD --> 1
+    # CENTRO --> 2
+    # SUD --> 3
 
     region_mapping = {
-        "Piemonte": 1, "Valle d'Aosta": 1, "Liguria": 1, "Lombardia": 1, "Trentino-Alto Adige": 1, "Veneto": 1, "Friuli venezia giulia": 1, "Emilia Romagna": 1,
-        "Toscana": 2, "Umbria": 2, "Marche": 2, "Lazio": 2,
-        "Abruzzo": 3, "Molise": 3, "Campania": 3, "Puglia": 3, "Basilicata": 3, "Calabria": 3, "Sicilia": 3, "Sardegna": 3
+        "Piemonte": 'Nord', "Valle d'Aosta": 'Nord', "Liguria": 'Nord', "Lombardia": 'Nord', "Trentino alto adige": 'Nord', "Veneto": 'Nord', "Friuli venezia giulia": 'Nord', "Emilia romagna": 'Nord', "Prov. auton. bolzano": 'Nord', "Prov. auton. trento": 'Nord',
+        "Toscana": 'Centro', "Umbria": 'Centro', "Marche": 'Centro', "Lazio": 'Centro',
+        "Abruzzo": 'Sud', "Molise": 'Sud', "Campania": 'Sud', "Puglia": 'Sud', "Basilicata": 'Sud', "Calabria": 'Sud', "Sicilia": 'Sud', "Sardegna": 'Sud'
     }
 
-    transformer.transform_col('regione_residenza', region_mapping)
-    transformer.transform_col('regione_erogazione', region_mapping)
+    transformer.transform_col('regione_residenza', region_mapping, 'zona_residenza')
+    transformer.transform_col('regione_erogazione', region_mapping, 'zona_erogazione')
+
+    print(df.info())
