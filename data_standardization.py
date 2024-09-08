d8010dfb4ed7c767ee859532d2fbbe6feb191b6d
Carlo Di Cicco <cdicicco2001@gmail.com>
2024-09-08 17:50:31 +0200

    Adattamento del main alle ultime modifiche e modifiche classe FeatureScaler per convertire le coordinate dei centoridi nei range originali.

--- data_standardization.py
+++ data_standardization.py
@@ -8,7 +8,34 @@ class FeatureScaler:
         pass
 
     @staticmethod
-    def standardize(df: pd.DataFrame, list_of_cols: list) -> pd.DataFrame:
+    def standardize_(df: pd.DataFrame, list_of_cols: list):
+        """
+        Standardize the columns in a DataFrame.
+        """
         for col in list_of_cols:
-            df[col] = (df[col] - df[col].mean()) / df[col].std()
+            mean = df[col].mean()
+            std = df[col].std()
+            df[col] = (df[col] - mean) / std
         return df
+
+    @staticmethod
+    def standardize(df: pd.DataFrame, list_of_cols: list) -> tuple[pd.DataFrame, dict, list]:
+        """
+        Standardize the columns in a DataFrame saving the mean and std for each column.
+        """
+        stats = {}  # Dizionario per salvare media e std
+        for col in list_of_cols:
+            mean = df[col].mean()
+            std = df[col].std()
+            stats[col] = {'mean': mean, 'std': std}  # Salva media e std
+            df[col] = (df[col] - mean) / std
+        return df, stats, list_of_cols
+
+    @staticmethod
+    def inverse_standardize_centroids(best_centroids: np.ndarray, stats: dict, list_of_cols: list) -> np.ndarray:
+        original_centroids = np.copy(best_centroids)  # Crea una copia dei centroidi per non modificarli direttamente
+        for i, col in enumerate(list_of_cols):
+            mean = stats[col]['mean']
+            std = stats[col]['std']
+            original_centroids[:, i] = (best_centroids[:, i] * std) + mean  # Applica l'operazione inversa
+        return original_centroids
