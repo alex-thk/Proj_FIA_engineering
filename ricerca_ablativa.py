6cf1e0940d14b0864c763b479ecfefe5446d8a86
Carlo Di Cicco <cdicicco2001@gmail.com>
2024-09-13 16:33:34 +0200

    implementazione della ricerca esaustiva.

--- ricerca_ablativa.py
+++ ricerca_ablativa.py
@@ -1,3 +1,5 @@
+import itertools
+
 import pandas as pd
 from data_reduction import DataReduction
 from data_cleaning import DataCleaning
@@ -104,13 +106,6 @@ print(df.columns)
 
 # Definiamo i diversi gruppi di feature da rimuovere in ogni iterazione
 ablative_groups = {
-    """'tutte': [],  # Prima iterazione con tutte le feature
-    'senza_prof_sanitario': [col for col in df.columns if col.startswith('codice_tipologia_professionista_sanitario')],
-    'senza_residenze': [col for col in df.columns if col.startswith('residenza_')],
-    'senza_sesso': ['sesso_female', 'sesso_male'],
-    'senza_struttura': [col for col in df.columns if col.startswith('codice_tipologia_struttura_erogazione')],
-    'senza_duration': ['duration_minutes'],
-    'senza_age': ['age']"""
     'prof_sanitario': [col for col in df.columns if col.startswith('codice_tipologia_professionista_sanitario')],
     'residenze': [col for col in df.columns if col.startswith('residenza_')],
     'sesso': ['sesso_female', 'sesso_male'],
@@ -124,138 +119,148 @@ performance = {}
 
 # Copia del dataset originale
 dataset_originale = df.copy()
+# Lista dei nomi dei gruppi
+group_names = list(ablative_groups.keys())
 
-# Iteriamo su ogni gruppo di feature nel dizionario ablative_groups
-for scenario, columns_to_remove in ablative_groups.items():
+# Iteriamo sulle combinazioni da 2 fino al massimo numero di gruppi
+for r in range(2, len(ablative_groups) + 1):
+    # Genera tutte le combinazioni di gruppi di numerosità 'r'
+    for combination in itertools.combinations(group_names, r):
+        # Otteniamo le feature da includere per questa combinazione
+        columns_to_include = []
+        for group in combination:
+            columns_to_include.extend(ablative_groups[group])
 
-    print(f"provo scenario {scenario}..")
-    # Manteniamo sempre le colonne 'semester' e 'year'
-    columns_to_keep = [col for col in dataset_originale.columns if col in columns_to_remove] + ['semester', 'year']
+        # Manteniamo sempre le colonne 'semester' e 'year'
+        columns_to_include += ['semester', 'year']
 
-    print(f"colonne che si hanno a questa iterazione {columns_to_keep}")
 
-    # Creiamo il subset delle feature
-    df = dataset_originale[columns_to_keep]
+        # Verifica se tutte le colonne esistono nel dataset
+        columns_to_include = [col for col in columns_to_include if col in dataset_originale.columns]
 
-    print(df.head())
+        print(f'Colonne che effettivamente si usano per questa iterazione: {columns_to_include}')
 
-    # Caricamento del dataset
-    dataset = df
+        # **Crea una copia del dataset con le colonne selezionate** per evitare modifiche dirette
+        df = dataset_originale[columns_to_include].copy()
 
-    # Inizializzazione dell'analizzatore di clustering
-    analyzer = ClusteringAnalyzer()
+        print(df.head())
 
-    # Suddivisione dei dati per anno e semestre
-    grouped_data = dataset.groupby(['year', 'semester'])
-    grouped_data_dict = {name: group.drop(columns=['year', 'semester']) for name, group in grouped_data}
+        # Caricamento del dataset
+        dataset = df
 
-    # Step 1: Determinazione del numero ottimale di cluster per ogni semestre
-    analyzer.find_optimal_k_for_semesters(grouped_data_dict)
-    # Dopo aver visualizzato i grafici, scegli manualmente il valore di k per ciascun semestre
-    optimal_k_for_semesters = {
-        1: 2,  # Valore ottimale per il primo semestre
-        2: 2  # Valore ottimale per il secondo semestre
-    }
+        # Inizializzazione dell'analizzatore di clustering
+        analyzer = ClusteringAnalyzer()
 
-    # Step 2: Valutare la stabilità delle feature tra anni diversi per ogni semestre
-    feature_stability = analyzer.evaluate_feature_stability_jsd(grouped_data_dict, optimal_k_for_semesters)
+        # Suddivisione dei dati per anno e semestre
+        grouped_data = dataset.groupby(['year', 'semester'])
+        grouped_data_dict = {name: group.drop(columns=['year', 'semester']) for name, group in grouped_data}
 
-    # Step 3: Seleziona le feature significative sulla base della stabilità
-    significant_features = analyzer.select_significant_features(feature_stability)
+        # Step 1: Determinazione del numero ottimale di cluster per ogni semestre
+        analyzer.find_optimal_k_for_semesters(grouped_data_dict)
+        # Dopo aver visualizzato i grafici, scegli manualmente il valore di k per ciascun semestre
+        optimal_k_for_semesters = {
+            1: 2,  # Valore ottimale per il primo semestre
+            2: 2  # Valore ottimale per il secondo semestre
+        }
 
-    # Step 4: Rifai il clustering usando solo le feature significative
-    clustering_results_significant = analyzer.apply_clustering_on_significant_features(
-        grouped_data_dict,
-        optimal_k_for_semesters,
-        significant_features
-    )
+        # Step 2: Valutare la stabilità delle feature tra anni diversi per ogni semestre
+        feature_stability = analyzer.evaluate_feature_stability_jsd(grouped_data_dict, optimal_k_for_semesters)
 
-    # Step 5: Calcola l'incremento nei cluster e crea il dataset finale con la nuova feature
-    # NOTA: Ora passa anche 'significant_features' alla funzione 'calculate_cluster_increment'
-    final_dataset_with_increments = analyzer.calculate_cluster_increment(
-        grouped_data_dict,
-        clustering_results_significant,
-        significant_features  # Passaggio delle feature significative
-    )
+        # Step 3: Seleziona le feature significative sulla base della stabilità
+        significant_features = analyzer.select_significant_features(feature_stability)
 
-    # Step 6: Aggiungi la colonna delle categorie di incremento
-    final_dataset_with_categories = analyzer.add_increment_category(final_dataset_with_increments)
+        # Step 4: Rifai il clustering usando solo le feature significative
+        clustering_results_significant = analyzer.apply_clustering_on_significant_features(
+            grouped_data_dict,
+            optimal_k_for_semesters,
+            significant_features
+        )
 
-    # Step 7: Salva il dataset finale con la colonna delle categorie
-    final_dataset_with_categories.to_csv('final_dataset_with_categories.csv', index=False)
+        # Step 5: Calcola l'incremento nei cluster e crea il dataset finale con la nuova feature
+        # NOTA: Ora passa anche 'significant_features' alla funzione 'calculate_cluster_increment'
+        final_dataset_with_increments = analyzer.calculate_cluster_increment(
+            grouped_data_dict,
+            clustering_results_significant,
+            significant_features  # Passaggio delle feature significative
+        )
 
-    # Visualizza il dataset finale
-    print("Dataset finale con incremento dei cluster e categorie:")
-    print(final_dataset_with_categories)
+        # Step 6: Aggiungi la colonna delle categorie di incremento
+        final_dataset_with_categories = analyzer.add_increment_category(final_dataset_with_increments)
 
-    # Mostra i valori unici della feature 'cluster_increment'
-    cluster_increment_values = final_dataset_with_increments['incremento numerico'].unique()
+        # Step 7: Salva il dataset finale con la colonna delle categorie
+        final_dataset_with_categories.to_csv('final_dataset_with_categories.csv', index=False)
 
-    # Visualizza i valori unici
-    print("Valori unici della feature 'cluster_increment':")
-    print(cluster_increment_values)
+        # Visualizza il dataset finale
+        print("Dataset finale con incremento dei cluster e categorie:")
+        print(final_dataset_with_categories)
 
-    dataframe_final = reducer.remove_insignificant_columns(final_dataset_with_increments, ['incremento numerico'])
-    # dataframe_final = final_dataset_with_increments.copy()
+        # Mostra i valori unici della feature 'cluster_increment'
+        cluster_increment_values = final_dataset_with_increments['incremento numerico'].unique()
 
-    # Definizione della griglia di iperparametri per il tuning
-    param_grid = {
-        'n_clusters': [5]  # numero di cluster che vogliamo testare
-    }
+        # Visualizza i valori unici
+        print("Valori unici della feature 'cluster_increment':")
+        print(cluster_increment_values)
 
-    print(final_dataset_with_increments.info())
+        dataframe_final = reducer.remove_insignificant_columns(final_dataset_with_increments, ['incremento numerico'])
+        # dataframe_final = final_dataset_with_increments.copy()
 
-    # Dizionario per mappare le stringhe ai numeri
-    mapping = {
-        'CONSTANT': 0,
-        'LOW': 1,
-        'MEDIUM': 2,
-        'HIGH': 3
-    }
+        # Definizione della griglia di iperparametri per il tuning
+        param_grid = {
+            'n_clusters': [5]  # numero di cluster che vogliamo testare
+        }
 
-    # Applicazione del mapping alla colonna 'incremento'
-    dataframe_final['incremento'] = dataframe_final['incremento'].map(mapping)
+        print(final_dataset_with_increments.info())
 
-    # Estrai la colonna "incremento" da final_dataset_with_increments e crea un array numpy 1D
-    labels = dataframe_final['incremento'].to_numpy()
+        # Dizionario per mappare le stringhe ai numeri
+        mapping = {
+            'CONSTANT': 0,
+            'LOW': 1,
+            'MEDIUM': 2,
+            'HIGH': 3
+        }
 
-    print("effettuo feature scaling per la variabile incremento")
+        # Applicazione del mapping alla colonna 'incremento'
+        dataframe_final['incremento'] = dataframe_final['incremento'].map(mapping)
 
-    dataframe_final = scaler.standardize_(dataframe_final, ['incremento'])
+        # Estrai la colonna "incremento" da final_dataset_with_increments e crea un array numpy 1D
+        labels = dataframe_final['incremento'].to_numpy()
 
-    # do più peso alla feature incremento
-    coefficiente = 2
-    dataframe_final['incremento'] = dataframe_final['incremento'] * coefficiente
+        print("effettuo feature scaling per la variabile incremento")
 
-    print("Istanzio classe HyperparameterTuner..")
+        dataframe_final = scaler.standardize_(dataframe_final, ['incremento'])
 
-    # Istanzia il tuner per KMeans
-    tuner = HyperparameterTuner(algorithm='kmeans', param_grid=param_grid, data=dataframe_final, labels=labels)
+        # do più peso alla feature incremento
+        coefficiente = 2
+        dataframe_final['incremento'] = dataframe_final['incremento'] * coefficiente
 
-    print("Inizio la ricerca dell'ottimo..")
+        print("Istanzio classe HyperparameterTuner..")
 
-    print("il dataframe finale è: ", dataframe_final)
-    print(f"i valori di incremento standardizzati sono {dataframe_final['incremento'].unique()}")
-    # Eseguiamo il Grid Search per trovare la combinazione migliore di iperparametri
-    results = tuner.perform_grid_search()
+        # Istanzia il tuner per KMeans
+        tuner = HyperparameterTuner(algorithm='kmeans', param_grid=param_grid, data=dataframe_final, labels=labels)
 
-    print("Grid search completato..")
+        print("Inizio la ricerca dell'ottimo..")
 
-    # Ora estrai i singoli valori dal dizionario `results`
-    best_params = results['best_params']
-    best_score = results['best_score']
-    best_labels = results['best_labels']
-    best_centroids = results['best_centroids']
+        print("il dataframe finale è: ", dataframe_final)
+        print(f"i valori di incremento standardizzati sono {dataframe_final['incremento'].unique()}")
+        # Eseguiamo il Grid Search per trovare la combinazione migliore di iperparametri
+        results = tuner.perform_grid_search()
 
-    print(f'lo score per lo scenario {scenario} è {best_score}')
+        print("Grid search completato..")
 
-    # Salviamo l'accuratezza nel dizionario performance
-    performance[scenario] = best_score
+        # Ora estrai i singoli valori dal dizionario `results`
+        best_params = results['best_params']
+        best_score = results['best_score']
+        best_labels = results['best_labels']
+        best_centroids = results['best_centroids']
+
+        # Salviamo l'accuratezza nel dizionario performance
+        combination_name = ', '.join(combination)
+        performance[combination_name] = best_score
+
+        print(f"Combinazione: {combination_name}, Performance: {best_score:.4f}")
 
 # Convertiamo i risultati in un DataFrame per analizzarli
-results_df = pd.DataFrame(list(performance.items()), columns=['Ablation_Scenario', 'Performance'])
-
-print(f'i risultati della ricerca ablativa sono {results_df}')
+results_df = pd.DataFrame(list(performance.items()), columns=['Combination', 'Performance'])
 
 # Salviamo results_df in un file JSON
 results_df.to_json('results_ricerca_ablativa.json', orient='records', lines=True)
